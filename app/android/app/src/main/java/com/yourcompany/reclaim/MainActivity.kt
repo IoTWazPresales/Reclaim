5c384bec9f06d787a5a9425116f478eb8f913bb9
Warren Eliason <warren.eliason@gmail.com>
2025-12-08 17:24:28 +0200

    Register Health Connect permission delegate

--- app/android/app/src/main/java/com/yourcompany/reclaim/MainActivity.kt
+++ app/android/app/src/main/java/com/yourcompany/reclaim/MainActivity.kt
@@ -17,8 +17,8 @@ class MainActivity : ReactActivity() {
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
-    super.onCreate(null)
     HealthConnectPermissionDelegate.setPermissionDelegate(this)
+    super.onCreate(null)
   }
 
   /**
